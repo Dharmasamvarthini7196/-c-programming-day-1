ac12e1ee75a929190252d76166be92c297fc3ea8
Dharmasamvarthini7196 <dharmasamvarthinis23aid@vetias.ac.in>
2025-05-13 12:59:16 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char arr[20]="happy coding"
+    char x[10]={'h','a','p','p','y','/0'}
+    printf("%c",arr[2]);
+    printf("\n%s\n",arr);
+    printf("%s",x)
+    return 0;
+
+
+    
+    }
+    
