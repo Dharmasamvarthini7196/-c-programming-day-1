ea18df3bce7e6a635c624fa8f8448fcaaa3f0faf
Dharmasamvarthini7196 <dharmasamvarthinis23aid@vetias.ac.in>
2025-05-14 15:27:02 +0530

    Add files via upload

---
+++ operation.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main(){
+    printf("%.2f",5.0/-5);
+   printf("\n%d",11%-4);
+   printf("\n%d",-11%-18);
+    printf("\n%d",-1%2+4*3);
+    printf("\n%d",3/3*3*3+2/2*3);
+    printf("\n%d",5+2/2%3+2*3);
+    printf("\n%f",5%-3);
+}
---
+++ problem2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+    {
+        int a=5, b=7,c;
+        c=a+b;
+        printf("addition %d" ,c);
+        return 0;
+   
+          
+
+}
+        
+        
+    
