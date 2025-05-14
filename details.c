b8c91614f5e9a7a061ae79d050b5ba555da8fbbf
Dharmasamvarthini7196 <dharmasamvarthinis23aid@vetias.ac.in>
2025-05-14 15:25:47 +0530

    Add files via upload

---
+++ details.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int age;
+   printf("enter your age :");
+   scanf("%d",&age);
+
+   float salary;
+   printf("enter your salary:");
+   scanf(" %f",&salary);
+
+    char name;
+     printf("name:");
+   scanf("/n your name:%s",name);
+   
+   }
+
+
+
+
